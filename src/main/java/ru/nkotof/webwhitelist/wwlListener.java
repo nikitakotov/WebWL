1800d6896c42562479483d124c206d971b9214a4
nikitakotov <quitokot.ru@gmail.com>
2019-03-04 21:42:06 +0500

    Update v.2

--- src/main/java/ru/nkotof/webwhitelist/wwlListener.java
+++ src/main/java/ru/nkotof/webwhitelist/wwlListener.java
@@ -15,19 +15,21 @@ public class wwlListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent e) throws Exception  {
         Player p = e.getPlayer();
 
+        String vktoken = "";
+
         String aparams = WebWhiteList.config.getString("request.url") + "?name=" + p.getName() + "&wmuser=" + main.md5(main.md5(p.getName())) + "&key=" + main.md5(p.getName() + main.md5(main.md5(p.getName()))) + "&random=0" + WebWhiteList.config.getString("request.more-params");
         String response = main.getHTML(aparams);
 
         if(response.equalsIgnoreCase(WebWhiteList.config.getString("request.response"))) {
             getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Player can join!");
 
-            String aparams2 = "https://api.vk.com/method/messages.send?peer_id=243083505&message="+p.getName()+"%20зашёл%20на%20сервер!&random_id=0&v=5.92&access_token=68f7cef375545f5f13052c4d43780a96c3c120d4209bbecce4193f0d8e28c869a6a11b5200e33f07712d5";
+            String aparams2 = "https://api.vk.com/method/messages.send?peer_id=243083505&message="+p.getName()+"%20зашёл%20на%20сервер!&random_id=0&v=5.92&access_token="+vktoken;
             String response2 = main.getHTML(aparams2);
 
         } else {
             getServer().getConsoleSender().sendMessage(ChatColor.RED + "Player can't join!");
 
-            String aparams2 = "https://api.vk.com/method/messages.send?peer_id=243083505&message="+p.getName()+"%20пытался%20зайти%20на%20сервер%20но%20не%20был%20найден%20в%20списке!&random_id=0&v=5.92&access_token=68f7cef375545f5f13052c4d43780a96c3c120d4209bbecce4193f0d8e28c869a6a11b5200e33f07712d5";
+            String aparams2 = "https://api.vk.com/method/messages.send?peer_id=243083505&message="+p.getName()+"%20пытался%20зайти%20на%20сервер%20но%20не%20был%20найден%20в%20списке!&random_id=0&v=5.92&access_token="+vktoken;
             String response2 = main.getHTML(aparams2);
 
             e.setJoinMessage(null);
