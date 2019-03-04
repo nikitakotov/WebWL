04971aa202868258b7d86a9c82167a560f37716b
nikitakotov <quitokot.ru@gmail.com>
2019-03-04 21:39:11 +0500

    Initial commit

---
+++ src/main/java/ru/nkotof/webwhitelist/WebWhiteList.java
@@ -0,0 +1,26 @@
+package ru.nkotof.webwhitelist;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WebWhiteList extends JavaPlugin {
+    
+    public static FileConfiguration config;
+    ConsoleCommandSender console = getServer().getConsoleSender();
+
+    @Override
+    public void onEnable() {
+        config = getConfig();
+        this.getServer().getPluginManager().registerEvents(new wwlListener(), this);
+        this.saveDefaultConfig();
+
+        console.sendMessage(ChatColor.GREEN + "WebWhiteList loaded!");
+    }
+
+    @Override
+    public void onDisable() {
+        console.sendMessage(ChatColor.GOLD + "WebWhiteList unloaded!");
+    }
+}
---
+++ src/main/java/ru/nkotof/webwhitelist/dev/main.java
@@ -0,0 +1,39 @@
+package ru.nkotof.webwhitelist.dev;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class main {
+    public static String md5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+        }
+        return null;
+    }
+    public static String getHTML(String Url) {
+        String line;
+        String result = "";
+        try {
+            URL url = new URL(Url);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            while ((line = rd.readLine()) != null) {
+                result += line;
+            }
+            rd.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
